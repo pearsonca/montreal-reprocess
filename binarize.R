916b272772e7237bb7d270143cc43110c2b66146
Carl A. B. Pearson <cap10@ufl.edu>
2015-10-28 11:23:34 -0400

    preprocessing job for montreal data

---
+++ binarize.R
@@ -0,0 +1,15 @@
+require(data.table)
+# should be run in output/src dir
+loclimits <- readRDS("../../input/locLimits.RData") # data.table, 
+cufiles <- list.files(pattern = "-cu")
+ccfiles <- list.files(pattern = "-cc")
+readIn <- function(f) setkey(setnames(fread(f), c("userA", "userB", "location_id", "login", "logout", "reason")), location_id)
+trim <- function(dt) merge(dt, loclimits, by="location_id")[!(login < start | end < logout), list(userA, userB, login, logout, reason), keyby=location_id]
+combos <- function(cu, cc) {
+  cu.dt <- trim(readIn(cu))
+  cc.dt <- trim(readIn(cc))
+  res <- setkey(rbind(cu.dt, cc.dt), login)
+  newfile <- sub("cu.csv","combos.RData", cu)
+  saveRDS(res, newfile)
+}
+mapply(combos, cufiles, ccfiles)
